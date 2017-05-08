7451f557aa380f5942ad8b51bf080614359d9fce
Astrear <primsark@gmail.com>
2017-05-08 17:50:54 -0500

    Need to fix the connection to facebook ...

---
+++ config/env/development.js
@@ -0,0 +1,9 @@
+module.exports = {
+	db: 'mongodb://localhost/mean-book',
+	sessionSecret: 'developmentSessionSecret',
+	facebook: {
+		clientID: '697455130456376',
+		clientSecret: '4813340f22bc3514c7d4f1a07f5283e4',
+		callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+	}
+};
