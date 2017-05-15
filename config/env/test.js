3bdfd849ce90726cf2e6645728356865369a5e36
Astrear <primsark@gmail.com>
2017-05-15 14:11:02 -0500

    Failed to run e2e tests

---
+++ config/env/test.js
@@ -0,0 +1,15 @@
+module.exports = {
+	db: 'mongodb://localhost/mean-test',
+	sessionSecret: 'Your Application Session Secret',
+	viewEngine: 'ejs',
+	facebook: {
+		clientID: '697455130456376',
+		clientSecret: '4813340f22bc3514c7d4f1a07f5283e4',
+		callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+	},
+	google: {
+		clientID: '1026911753571-akvn86hv0t43gcu5d84s0bp2e6kcaq7t.apps.googleusercontent.com',
+		clientSecret: 'FO3oSpQVB7eRcJRmvBvR8wR5',
+		callbackURL: 'http://wolfamong.com:3000/oauth/google/callback'
+	}
+};
---
+++ config/protractor.js
@@ -0,0 +1,4 @@
+exports.config = {
+	seleniumAddress: 'http://localhost:4444/wd/hub',
+	specs: ['public/articles/tests/e2e/aticles.client.e2e.js']
+}
